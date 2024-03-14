b20e4a384add65b439b9013d0c28c75a3f940a44
Markus Seidl <mark@bearo.de>
2024-03-14 15:08:17 +0100

    WIP

--- dataset/llama_summarizer.py
+++ dataset/llama_summarizer.py
@@ -12,8 +12,8 @@ import urllib.request
 MODEL_DL = "https://huggingface.co/TheBloke/Wizard-Vicuna-30B-Uncensored-GGUF/resolve/main/Wizard-Vicuna-30B-Uncensored.Q5_K_S.gguf"
 MODEL_NAME = "Wizard-Vicuna-30B-Uncensored.Q5_K_S.gguf"
 
-# MODEL_DL = "https://huggingface.co/TheBloke/TinyLlama-1.1B-intermediate-step-1431k-3T-GGUF/resolve/main/tinyllama-1.1b-intermediate-step-1431k-3t.Q5_K_S.gguf"
-# MODEL_NAME = "tinyllama-1.1b-intermediate-step-1431k-3t.Q5_K_S.gguf"
+MODEL_DL = "https://huggingface.co/TheBloke/TinyLlama-1.1B-intermediate-step-1431k-3T-GGUF/resolve/main/tinyllama-1.1b-intermediate-step-1431k-3t.Q5_K_S.gguf"
+MODEL_NAME = "tinyllama-1.1b-intermediate-step-1431k-3t.Q5_K_S.gguf"
 
 MODEL_PATH = os.path.dirname(os.path.abspath(__file__)) + "/../temp/"
 MODEL = None
@@ -23,6 +23,8 @@ TEMPLATE = "A chat between a curious user and an artificial intelligence assista
 
 def download_model():
     print(f"Downloading summarizer model: {MODEL_NAME}")
+    if os.path.exists(MODEL_PATH + MODEL_NAME):
+        return
     urllib.request.urlretrieve(MODEL_DL, MODEL_PATH + MODEL_NAME)
 
 
@@ -43,3 +45,7 @@ def summarize_text(text, length):
             f"Extract the three key points from the following text and summarize it into {length} sentences: {text}"
         )
     return response
+
+
+if __name__ == "__main__":
+    download_model()
