9d512815f9c686c32e1c227ee00b8ff57feb96c7
Markus Seidl <mark@bearo.de>
2024-03-23 09:13:50 +0100

    WIP

--- docker/onstart.sh
+++ docker/onstart.sh
@@ -7,12 +7,12 @@ export CUDA_HOME=/usr/local/cuda/
 
 # export LLAMACPP_VERBOSE="true"
 
-echo "Rebuilding LLAMA CPP"
-cd /home/llama.cpp/
-export LLAMA_CUBLAS=on
-make clean && make libllama.so
-pip uninstall llama-cpp-python
-CMAKE_ARGS="-DLLAMA_CLBLAST=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
+#echo "Rebuilding LLAMA CPP"
+#cd /home/llama.cpp/
+#export LLAMA_CUBLAS=on
+#make clean && make libllama.so
+#pip uninstall llama-cpp-python
+#CMAKE_ARGS="-DLLAMA_CLBLAST=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
 
 cd /home/novel-ai/dataset/
 
