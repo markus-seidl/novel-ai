19fc8bbb996a966242c7e14e8e3448a8c1de3ebe
Markus Seidl <mark@bearo.de>
2024-03-23 09:16:09 +0100

    WIP

--- docker/onstart.sh
+++ docker/onstart.sh
@@ -7,14 +7,21 @@ export CUDA_HOME=/usr/local/cuda/
 
 # export LLAMACPP_VERBOSE="true"
 
-#echo "Rebuilding LLAMA CPP"
-#cd /home/llama.cpp/
-#export LLAMA_CUBLAS=on
-#make clean && make libllama.so
-#pip uninstall llama-cpp-python
-#CMAKE_ARGS="-DLLAMA_CLBLAST=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
-
 cd /home/novel-ai/dataset/
 
 pip install -r requirements.txt
+
+
+python3 generator_llm_2.py
+
+# Assume python segfaulted, try to recompile and do it again
+touch /home/segfaulted_retry
+
+echo "Rebuilding LLAMA CPP"
+cd /home/llama.cpp/
+export LLAMA_CUBLAS=on
+make clean && make libllama.so
+pip uninstall llama-cpp-python
+CMAKE_ARGS="-DLLAMA_CLBLAST=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
+
 python3 generator_llm_2.py
